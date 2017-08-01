4e6694548faa1a06bf87de38bcecc388d69db592
hristouzunovaz <hristouzunovaz@gmail.com>
2017-08-01 23:06:29 +0100

    Network and object mapping

---
+++ CatchTest/CatchTest/ItemsViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ItemsViewController.swift
+//  CatchTest
+//
+//  Created by Hristo Uzunov on 01/08/2017.
+//  Copyright © 2017 HristoUzunov. All rights reserved.
+//
+
+import UIKit
+
+class ItemsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        /*NetworkAssistant.shared.fetchItems(from: itemsURL) { (items) in
+            print(items)
+        }*/
+    }
+
+}
